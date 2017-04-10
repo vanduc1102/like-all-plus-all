a48cc0db36fd51b5e05fcb8357128fb23c8738f4
Duc Nguyen <vanducld@gmail.com>
2017-04-10 23:01:40 +0700

    #8 - correct youtube like video

--- source/scripts/youtube.js
+++ source/scripts/youtube.js
@@ -2,10 +2,10 @@ var ytTimeout = setTimeout(function() {
     var button = $("button.like-button-renderer-like-button-unclicked");
     if (button.hasClass("hid")) {
         log.debug("button is pressed, clearInterval : " + ytTimeout);
+        sendAnalytic("youtube-autolike");
         clearTimeout(ytTimeout);
     } else {
         log.debug("Button is not pressed, try to press after 3 second.");
-        sendAnalytic("youtube-autolike");
         button.click();
     }
 }, 3000);
