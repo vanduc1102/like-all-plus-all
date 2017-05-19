735421209b23fc491ecf08e08a1049f4cfe76f4e
Duc NGUYEN <vanducld@gmail.com>
2017-05-20 02:21:51 +0700

    Moved fire event to utils

--- source/scripts/content_script.js
+++ source/scripts/content_script.js
@@ -386,61 +386,6 @@ function isNewGooglePlus() {
 }
 
 function triggerClickEvent(node) {
-    fireEvent(node, "mousedown");
-    fireEvent(node, "mouseup");
+    Utils.fireEvent(node, "mousedown");
+    Utils.fireEvent(node, "mouseup");
 }
-
-function fireEvent(node, eventName) {
-    log.debug("content_script - fireEvent");
-    // Make sure we use the ownerDocument from the provided node to avoid cross-window problems
-    var doc;
-    if (node.ownerDocument) {
-        doc = node.ownerDocument;
-    } else if (node.nodeType == 9) {
-        // the node may be the document itself, nodeType 9 = DOCUMENT_NODE
-        doc = node;
-    } else {
-        throw new Error("Invalid node passed to fireEvent: " + node.id);
-    }
-
-    if (node.dispatchEvent) {
-        // Gecko-style approach (now the standard) takes more work
-        var eventClass = "";
-
-        // Different events have different event classes.
-        // If this switch statement can't map an eventName to an eventClass,
-        // the event firing is going to fail.
-        switch (eventName) {
-            case "click": // Dispatching of 'click' appears to not work correctly in Safari. Use 'mousedown' or 'mouseup' instead.
-            case "mousedown":
-            case "mouseup":
-                eventClass = "MouseEvents";
-                break;
-
-            case "focus":
-            case "change":
-            case "blur":
-            case "select":
-                eventClass = "HTMLEvents";
-                break;
-
-            default:
-                throw "fireEvent: Couldn't find an event class for event '" + eventName + "'.";
-                break;
-        }
-        var event = doc.createEvent(eventClass);
-
-        var bubbles = eventName == "change" ? false : true;
-        event.initEvent(eventName, bubbles, true); // All events created as bubbling and cancelable.
-
-        event.synthetic = true; // allow detection of synthetic events
-        // The second parameter says go ahead with the default action
-        node.dispatchEvent(event, true);
-    } else if (node.fireEvent) {
-        // IE-old school style
-        var event = doc.createEventObject();
-        event.synthetic = true; // allow detection of synthetic events
-        node.fireEvent("on" + eventName, event);
-    }
-
-};
--- source/scripts/utils.js
+++ source/scripts/utils.js
@@ -7,6 +7,10 @@ var DEBUG = false;
 var CLICK_BUTTON = true;
 var log = {};
 
+var Utils = {
+    fireEvent: fireEvent
+};
+
 log.ASSERT = 1;
 log.ERROR = 2;
 log.WARN = 3;
@@ -402,3 +406,58 @@ function removeRunningBackgroundColor() {
     }, 3000);
 
 }
+
+function fireEvent(node, eventName) {
+    log.debug("content_script - fireEvent");
+    // Make sure we use the ownerDocument from the provided node to avoid cross-window problems
+    var doc;
+    if (node.ownerDocument) {
+        doc = node.ownerDocument;
+    } else if (node.nodeType == 9) {
+        // the node may be the document itself, nodeType 9 = DOCUMENT_NODE
+        doc = node;
+    } else {
+        throw new Error("Invalid node passed to fireEvent: " + node.id);
+    }
+
+    if (node.dispatchEvent) {
+        // Gecko-style approach (now the standard) takes more work
+        var eventClass = "";
+
+        // Different events have different event classes.
+        // If this switch statement can't map an eventName to an eventClass,
+        // the event firing is going to fail.
+        switch (eventName) {
+            case "click": // Dispatching of 'click' appears to not work correctly in Safari. Use 'mousedown' or 'mouseup' instead.
+            case "mousedown":
+            case "mouseup":
+                eventClass = "MouseEvents";
+                break;
+
+            case "focus":
+            case "change":
+            case "blur":
+            case "select":
+                eventClass = "HTMLEvents";
+                break;
+
+            default:
+                throw "fireEvent: Couldn't find an event class for event '" + eventName + "'.";
+                break;
+        }
+        var event = doc.createEvent(eventClass);
+
+        var bubbles = eventName == "change" ? false : true;
+        event.initEvent(eventName, bubbles, true); // All events created as bubbling and cancelable.
+
+        event.synthetic = true; // allow detection of synthetic events
+        // The second parameter says go ahead with the default action
+        node.dispatchEvent(event, true);
+    } else if (node.fireEvent) {
+        // IE-old school style
+        var event = doc.createEventObject();
+        event.synthetic = true; // allow detection of synthetic events
+        node.fireEvent("on" + eventName, event);
+    }
+
+};
